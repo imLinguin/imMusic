59fe8190df5db996fd2a51f878142e0556d7723c
imLinguin <62100117+imLinguin@users.noreply.github.com>
2020-11-09 14:45:12 +0100

    Node 12 fix

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "nowplaying",
   aliases: ["np"],
   run(message, args, client) {
-    let song = client.queues.get(message.guild.id)?.tracks[0];
+    let song = client.queues.get(message.guild.id).tracks[0];
     if (!song) return message.channel.send("No music playing currently!");
     let embed = {
       color: 0x51cab0,
